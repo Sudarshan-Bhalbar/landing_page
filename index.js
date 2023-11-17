c9ac6256ccff89de7d0b0faa5ea065ae32359256
Sudarshan_Bhalbar <sudarshan.bhalbar@gmail.com>
2023-11-17 19:19:38 +0530

    completed target
    target --->To Learn  Scroll Reveal CDN

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ closeBtn.addEventListener("click",()=>{
   menu.classList.add("-top-96")
   menu.classList.remove("top-0");
 })
+
+
+const sr = ScrollReveal({
+  distance:'90px',
+  duration:3000,
+})
+
+sr.reveal('.home_data',{origin:'top',delay:400})
+sr.reveal('.home_img',{origin:'bottom',delay:600})
